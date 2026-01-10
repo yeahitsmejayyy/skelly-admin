56e29fc5736020bf3b30cbaa4c2c6fc5fd2d44ed
yeahitsmejayyy <hello@itsjayyy.com>
2026-01-09 17:11:39 -0800

    chore(admin): minor changes to display

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="flex min-h-svh flex-col items-center justify-center gap-4 p-4">
       <h1 className="text-2xl flex flex-col items-center gap-1">
         <span className="flex items-center justify-center border bg-background rounded-full w-[46px] h-[46px]">💀</span>
-        <span className="font-bold">@skelly/admin</span>
+        <span className="font-bold">skelly-admin</span>
       </h1>
       <div className="mx-auto"><ModeToggle /></div>
       {healthCheck.data ? (
